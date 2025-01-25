91ab90b8eba5a4f77a687d6372866fe4b2935acc
PauloCesar53 <pcdilauro53@gmail.com>
2025-01-25 12:23:26 -0300

    Primeiro commit

--- include/ani_00.h
+++ include/ani_00.h
@@ -1,11 +1,7 @@
-#ifndef ANI_00_H
-#define ANI_00_H
+#ifndef ANI_04_H
+#define ANI_04_H
 
 //Declaração das funções da animação
-void ani_00();
-void npInit(uint pin);
-void npSetLED(const uint index, const uint8_t r, const uint8_t g, const uint8_t b);
-void npClear();
-void npWrite();
+void ani_04();
 
 #endif
