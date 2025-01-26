3262ca78c4185de0dac6a9dfd5a38a481357550f
marques-svnt <marques.svnt.002@gmail.com>
2025-01-25 21:46:51 -0300

    Criação de biblioteca para os leds

--- main.c
+++ main.c
@@ -7,45 +7,79 @@
 #include "ani_asterisco.h"
 #include "teclado.h"
 #include "buzzer.h"
+#include "leds.h"
 
 // Função principal
-int main() {
+int main()
+{
     stdio_init_all();
     teclado();
 
-    while (true) {
+    while (true)
+    {
         char key = pico_keypad_get_key();
 
         // Controle baseado em teclas específicas
-        if (key == '0') {
+        if (key == '0')
+        {
             ani_00();
-        } else if (key == '1') {
-            //Animação da tecla 1
-        } else if (key == '2') {
-            //Animação da tecla 2
-        } else if (key == '3') {
-            //Animação da tecla 3
-        } else if (key == '4') {
+            sleep_ms(500);
+        }
+        else if (key == '1')
+        {
+            // Animação da tecla 1
+        }
+        else if (key == '2')
+        {
+            // Animação da tecla 2
+        }
+        else if (key == '3')
+        {
+            // Animação da tecla 3
+        }
+        else if (key == '4')
+        {
             ani_04();
-        } else if (key == '5') {
-            //Animação da tecla 5
-        } else if (key == '6') {
-            //Animação da tecla 6
-        } else if (key == '7') {
-            //Animação da tecla 7
-        } else if (key == 'A') {
-            //Animação da tecla A
-        } else if (key == 'B') {
-            //Animação da tecla B
-        } else if (key == 'C') {
-            //Animação da tecla C
-        } else if (key == 'D') {
-            //Animação da tecla D
-        } else if (key == '#') {
-            //Animação da tecla #
-        } else if (key == '*') {
-            ani_asterisco();  // Chama a funcionalidade da tecla '*'
-        } else {
+            sleep_ms(500);
+        }
+        else if (key == '5')
+        {
+            // Animação da tecla 5
+        }
+        else if (key == '6')
+        {
+            // Animação da tecla 6
+        }
+        else if (key == '7')
+        {
+            // Animação da tecla 7
+        }
+        else if (key == 'A')
+        {
+            // Animação da tecla A
+        }
+        else if (key == 'B')
+        {
+            // Animação da tecla B
+        }
+        else if (key == 'C')
+        {
+            // Animação da tecla C
+        }
+        else if (key == 'D')
+        {
+            // Animação da tecla D
+        }
+        else if (key == '#')
+        {
+            // Animação da tecla #
+        }
+        else if (key == '*')
+        {
+            ani_asterisco(); // Chama a funcionalidade da tecla '*'
+        }
+        else
+        {
             printf("Essa tecla não possui animação");
         }
     }
--- src/ani_04.c
+++ src/ani_04.c
@@ -2,7 +2,7 @@
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
-#include "ani_00.h"
+#include "leds.h"
 
 // Definição do número de LEDs e pino.
 #define LED_COUNT 25
@@ -10,10 +10,10 @@
 
 // Definição das cores:
 #define BRANCO 200, 200, 200
-#define AZUL  0, 0, 200
+#define AZUL 0, 0, 200
 
 // Definição do delay
-#define delay 1000/4 
+#define delay 1000 / 4
 
 /// animação 4 caso seja apertado a tecla 4
 void ani_04()
@@ -24,105 +24,106 @@ void ani_04()
   // Inicializa matriz de LEDs NeoPixel.
   npInit(LED_PIN);
   npClear();
-    // frame 1
-    npSetLED(1, AZUL);
-    npSetLED(3, AZUL);
-    npSetLED(6, AZUL);
-    npSetLED(7, AZUL);
-    npSetLED(8, AZUL);
-    npSetLED(12, AZUL);
-    npWrite();
-    sleep_ms(delay);
-    npClear();
+  // frame 1
+  npSetLED(1, AZUL);
+  npSetLED(3, AZUL);
+  npSetLED(6, AZUL);
+  npSetLED(7, AZUL);
+  npSetLED(8, AZUL);
+  npSetLED(12, AZUL);
+  npWrite();
+  sleep_ms(delay);
+  npClear();
 
-    // frame 2
-    npSetLED(1, AZUL);
-    npSetLED(3, AZUL);
-    npSetLED(6, AZUL);
-    npSetLED(7, AZUL);
-    npSetLED(8, AZUL);
-    npSetLED(12, AZUL);
-    npSetLED(17, BRANCO);
-    npWrite();
-    sleep_ms(delay);
-    npClear();
+  // frame 2
+  npSetLED(1, AZUL);
+  npSetLED(3, AZUL);
+  npSetLED(6, AZUL);
+  npSetLED(7, AZUL);
+  npSetLED(8, AZUL);
+  npSetLED(12, AZUL);
+  npSetLED(17, BRANCO);
+  npWrite();
+  sleep_ms(delay);
+  npClear();
 
-    // frame 3
-    npSetLED(1, AZUL);
-    npSetLED(3, AZUL);
-    npSetLED(6, AZUL);
-    npSetLED(7, AZUL);
-    npSetLED(8, AZUL);
-    npSetLED(12, AZUL);
-    npSetLED(22, BRANCO);
-    npWrite();
-    sleep_ms(delay);
-    npClear();
+  // frame 3
+  npSetLED(1, AZUL);
+  npSetLED(3, AZUL);
+  npSetLED(6, AZUL);
+  npSetLED(7, AZUL);
+  npSetLED(8, AZUL);
+  npSetLED(12, AZUL);
+  npSetLED(22, BRANCO);
+  npWrite();
+  sleep_ms(delay);
+  npClear();
 
-    // frame 4
-    npSetLED(1, AZUL);
-    npSetLED(3, AZUL);
-    npSetLED(6, AZUL);
-    npSetLED(7, AZUL);
-    npSetLED(8, AZUL);
-    npSetLED(12, AZUL);
-    npWrite();
-    sleep_ms(delay);
-    npClear();
+  // frame 4
+  npSetLED(1, AZUL);
+  npSetLED(3, AZUL);
+  npSetLED(6, AZUL);
+  npSetLED(7, AZUL);
+  npSetLED(8, AZUL);
+  npSetLED(12, AZUL);
+  npWrite();
+  sleep_ms(delay);
+  npClear();
 
-    // frame 5
-    npSetLED(0, AZUL);
-    npSetLED(2, AZUL);
-    npSetLED(7, AZUL);
-    npSetLED(8, AZUL);
-    npSetLED(9, AZUL);
-    npSetLED(11, AZUL);
-    npWrite();
-    sleep_ms(delay);
-    npClear();
+  // frame 5
+  npSetLED(0, AZUL);
+  npSetLED(2, AZUL);
+  npSetLED(7, AZUL);
+  npSetLED(8, AZUL);
+  npSetLED(9, AZUL);
+  npSetLED(11, AZUL);
+  npWrite();
+  sleep_ms(delay);
+  npClear();
 
-    // frame 6
-    npSetLED(0, AZUL);
-    npSetLED(2, AZUL);
-    npSetLED(7, AZUL);
-    npSetLED(8, AZUL);
-    npSetLED(9, AZUL);
-    npSetLED(11, AZUL);
-    npSetLED(18, BRANCO);
-    npWrite();
-    sleep_ms(delay);
-    npClear();
+  // frame 6
+  npSetLED(0, AZUL);
+  npSetLED(2, AZUL);
+  npSetLED(7, AZUL);
+  npSetLED(8, AZUL);
+  npSetLED(9, AZUL);
+  npSetLED(11, AZUL);
+  npSetLED(18, BRANCO);
+  npWrite();
+  sleep_ms(delay);
+  npClear();
 
-    // frame 7
-    npSetLED(0, AZUL);
-    npSetLED(2, AZUL);
-    npSetLED(7, AZUL);
-    npSetLED(8, AZUL);
-    npSetLED(9, AZUL);
-    npSetLED(11, AZUL);
-    npSetLED(21, BRANCO);
-    npWrite();
-    sleep_ms(delay);
-    npClear();
+  // frame 7
+  npSetLED(0, AZUL);
+  npSetLED(2, AZUL);
+  npSetLED(7, AZUL);
+  npSetLED(8, AZUL);
+  npSetLED(9, AZUL);
+  npSetLED(11, AZUL);
+  npSetLED(21, BRANCO);
+  npWrite();
+  sleep_ms(delay);
+  npClear();
 
-    // frame 8
-    npSetLED(1, AZUL);
-    npSetLED(3, AZUL);
-    npSetLED(6, AZUL);
-    npSetLED(7, AZUL);
-    npSetLED(8, AZUL);
-    npSetLED(12, AZUL);
-    npWrite();
-    sleep_ms(delay);
-    npClear();
+  // frame 8
+  npSetLED(1, AZUL);
+  npSetLED(3, AZUL);
+  npSetLED(6, AZUL);
+  npSetLED(7, AZUL);
+  npSetLED(8, AZUL);
+  npSetLED(12, AZUL);
+  npWrite();
+  sleep_ms(delay);
+  npClear();
 
-    // frame 9
-    npClear();
-    npWrite();
-    sleep_ms(delay);
+  // frame 9
+  npClear();
+  npWrite();
+  sleep_ms(delay);
 }
-//caso seja apertado a tecla A todos os leds serão desligados
-void bot_A(){
+// caso seja apertado a tecla A todos os leds serão desligados
+void bot_A()
+{
   npClear();
   npWrite();
   sleep_ms(delay);
