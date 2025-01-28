bceda6549032271e7fcf2f7d5a5ed81f799579a6
brunaalvescabral <brunaalvescabralb36@gmail.com>
2025-01-28 15:32:26 -0300

    Adição da biblioteca pico/stdlib.h no arquivo leds.h

--- include/leds.h
+++ include/leds.h
@@ -1,6 +1,8 @@
 #ifndef leds_H
 #define leds_H
 
+#include "pico/stdlib.h" 
+
 //Declaração das funções dos leds
 void npInit(uint pin);
 void npSetLED(const uint index, const uint8_t r, const uint8_t g, const uint8_t b);
