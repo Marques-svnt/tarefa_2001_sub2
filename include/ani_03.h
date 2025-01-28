a02c481cd8c624c3273cc2ec8d9db37cd89feff7
Ma756-max <marcelorick2004@gmail.com>
2025-01-28 09:09:53 -0300

    Adicionei animação para o botão 3 (ani_03.h) com controle de leds

---
+++ include/ani_03.h
@@ -0,0 +1,18 @@
+#ifndef ANI_03_H
+#define ANI_03_H
+
+
+
+/
+// Declaração da função de animação para o botão 3
+void ani_03();
+
+v
+void npInit(uint pin);
+
+vo
+void npSetLED(const uint index, const uint8_t r, const uint8_t g, const uint8_t b);
+void npClear();
+void npWrite();
+
+#endif
